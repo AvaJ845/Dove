6d039fa0042db7ee3cfce3f4a62e5048bc9251be
Dj <avaresearchllc@gmail.com>
2025-03-19 17:41:35 -0400

    Create  __init__.py
    3.19.25 DJ Package initializer

---
+++ dove_app/modules/ __init__.py
@@ -0,0 +1,12 @@
+# modules/__init__.py
+# Makes modules a package
+
+# Import all modules to make them accessible through the package
+from . import data
+from . import calculations
+from . import visualizations
+from . import projections
+from . import ui_components
+from . import about
+from . import database
+from . import portfolio_manager
